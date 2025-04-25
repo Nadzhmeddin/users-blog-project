18aed7b65ac573e83ee1c024bff7e3dd3e188422
Nadzhmeddin <xside55@yandex.ru>
2025-04-25 11:01:17 +0300

    Create project users-blog

---
+++ src/main/java/ru/users_blog/UsersBlogApplication.java
@@ -0,0 +1,13 @@
+package ru.users_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/users_blog/UsersBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.users_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
