5a3e8d79c844358bcdfea0d85afe12ac34e215b8
Nadzhmeddin <xside55@yandex.ru>
2025-04-28 00:21:36 +0300

    Writer post controller and user controller layers. Checked all in postman, worked!

--- src/main/java/ru/users_blog/controller/CommentController.java
+++ src/main/java/ru/users_blog/controller/CommentController.java
@@ -1,4 +1,24 @@
 package ru.users_blog.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.users_blog.dto.CommentDto;
+import ru.users_blog.service.impl.CommentServiceImpl;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/comments")
+@RequiredArgsConstructor
 public class CommentController {
+
+    private final CommentServiceImpl service;
+
+    @GetMapping
+    public ResponseEntity<List<CommentDto>> findAll() {
+
+    }
 }
--- src/main/java/ru/users_blog/controller/PostController.java
+++ src/main/java/ru/users_blog/controller/PostController.java
@@ -1,4 +1,52 @@
 package ru.users_blog.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.users_blog.dto.PostDto;
+import ru.users_blog.service.impl.PostServiceImpl;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostServiceImpl service;
+
+    @GetMapping
+    public ResponseEntity<List<PostDto>> findAll() {
+        List<PostDto> allPosts = service.findAll();
+        if(allPosts.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } else return ResponseEntity.status(HttpStatus.OK).body(allPosts);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<PostDto>> findById(@PathVariable Long id) {
+        Optional<PostDto> foundPost = service.findById(id);
+        if(foundPost.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(foundPost);
+        } else return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PostMapping
+    public ResponseEntity<PostDto> save(@RequestBody PostDto postDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(postDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        service.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteAll() {
+        service.deleteAll();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
