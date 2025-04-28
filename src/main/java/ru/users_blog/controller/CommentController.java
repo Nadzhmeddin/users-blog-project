c61b9d25be2b3ff192ee3cf66fdff967805745e7
Nadzhmeddin <xside55@yandex.ru>
2025-04-28 10:57:51 +0300

    Writed comment controller and logic. All works good. Next write security and exception handler.

--- src/main/java/ru/users_blog/controller/CommentController.java
+++ src/main/java/ru/users_blog/controller/CommentController.java
@@ -1,14 +1,15 @@
 package ru.users_blog.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.users_blog.dto.CommentDto;
+import ru.users_blog.mapper.CommentMapper;
 import ru.users_blog.service.impl.CommentServiceImpl;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/comments")
@@ -19,6 +20,34 @@ public class CommentController {
 
     @GetMapping
     public ResponseEntity<List<CommentDto>> findAll() {
+        List<CommentDto> allComments = service.findAll();
+        if(allComments.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } else return ResponseEntity.status(HttpStatus.OK).body(allComments);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<CommentDto>> findById(@PathVariable Long id) {
+        Optional<CommentDto> foundComment = service.findById(id);
+        if(foundComment.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(foundComment);
+        } else return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PostMapping
+    public ResponseEntity<CommentDto> save(@RequestBody CommentDto commentDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(commentDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        service.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteAll() {
+        service.deleteAll();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
