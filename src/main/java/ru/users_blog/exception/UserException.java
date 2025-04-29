d4d79870225d5c3f8ae08e9341ba867e78baf2a3
Nadzhmeddin <xside55@yandex.ru>
2025-04-29 19:15:14 +0300

    Writed exception

--- src/main/java/ru/users_blog/exception/UserException.java
+++ src/main/java/ru/users_blog/exception/UserException.java
@@ -1,9 +1,13 @@
 package ru.users_blog.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-
+@AllArgsConstructor
 @Getter
-public record UserException(String message, Throwable throwable, HttpStatus httpStatus) {
+public class UserException {
+    private final String message;
+    private final Throwable throwable;
+    private final HttpStatus httpStatus;
 }
